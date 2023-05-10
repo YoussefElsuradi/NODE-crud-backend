57726315516859a59e104f2a9c45d0d42ecc4be1
Mike <minp4545@gmail.com>
2023-05-10 18:50:37 -0400

    Added assignto to task

--- index.js
+++ index.js
@@ -9,11 +9,18 @@ app.use(cors());
 app.use(express.json());
 
 // specific routes
-const sequelize = new Sequelize('railway', 'postgres', 'SZLWM5zIVmSKLeCafhBJ', {
-    host: 'containers-us-west-16.railway.app',
+const sequelize = new Sequelize('dfvlo44fqd2221', 'vmnssahmeabbak', '9ba166642a6071c500130b68397498fbf621a0a7df156a57f5923e78eeb1d79c', {
+    host: 'ec2-34-193-110-25.compute-1.amazonaws.com',
     dialect: 'postgres',
-    port: 7249
-});
+    port: 5432,
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // You may need to set this to true on production
+      }
+    }
+  });
 
 sequelize.authenticate()
   .then(() => {
